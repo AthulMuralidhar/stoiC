a33b938ff2517d47f1e479a0c92f4da2768db864
Athul Muralidhar <athul.m68@gmail.com>
2019-06-11 12:38:14 +0200

    datetime ++

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,29 @@ void main(){
   ));
 }
 
+
 class HomeScreen extends StatelessWidget{
+  final date = new DateTime.now().toString();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Center(
-        child: Text('The counter for stoics'),
+      body: Column(
+        children: <Widget>[
+          Center(
+            child: Text('a practical counter for practicing stoics'),
+            widthFactor: 2.0,
+            heightFactor: 2.0,
+          ),
+          Center(
+              child: Text(date),
+              widthFactor: 2.0,
+              heightFactor: 2.0,
+          ),
+        ],
       ),
     );
   }
