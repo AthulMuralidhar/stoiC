c1d98980df57a8c3944e32f8c1cd3741f7bcfbcc
Athul Muralidhar <athul.m68@gmail.com>
2019-06-20 16:27:18 +0200

    Added navigation and an overview page

--- lib/main.dart
+++ lib/main.dart
@@ -23,13 +23,57 @@ class HomeScreen extends StatelessWidget{
             widthFactor: 2.0,
             heightFactor: 2.0,
           ),
-          Center(
-            child: Text('place holder'),
+          Align(
+            alignment: Alignment.center,
+            child: Text('''
+            In your daily travels, you might 
+            come across various situations that 
+            try to put your stoicabilities to 
+            the test. Here is a helper app to 
+            distinguish and keep track of the 
+            internals, the "real" things that
+            you actually have to care about 
+           '''
+           ),
             widthFactor: 2.0,
             heightFactor: 2.0,
+          ),
+          Center(
+            child: RaisedButton(
+                onPressed: (){
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context)=>OverviewScreen()),
+                  );
+                },
+              child: Text('Go!'),
+            ),
           ),
         ],
       )
       );
+  }
+}
+
+class OverviewScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title:Text('Overview screen!'),
+    ),
+    body: Center(
+    child: Column(
+    children: <Widget>[
+      RaisedButton(
+        child: Text('Go back'),
+        onPressed: (){
+          Navigator.pop(context);
+          },
+        ),
+      ],
+    ),
+    ),
+    );
   }
 }
